2307a67e4a31c348a304febd235b6599cc83b40d
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-11-26 16:15:53 -0600

    update version

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ namespace CustomUI
     public class Plugin : IPlugin
     {
         public string Name => "BeatSaberCustomUI";
-        public string Version => "0.0.1";
+        public string Version => "0.1.0";
         public void OnApplicationStart()
         {
         }
