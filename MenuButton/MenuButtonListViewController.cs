67c48b33836251b21ba143245ea7f79593d96bd1
Robyn <slovett@hotmail.co.uk>
2019-01-18 00:57:45 +0000

    Support for MenuButton list and button pinning/unpinning

---
+++ MenuButton/MenuButtonListViewController.cs
@@ -0,0 +1,141 @@
+﻿using CustomUI.BeatSaber;
+using CustomUI.Utilities;
+using HMUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using VRUI;
+
+namespace CustomUI.MenuButton
+{
+    class MenuButtonListViewController : CustomListViewController, TableView.IDataSource
+    {
+        private List<MenuButton> buttons;
+
+        readonly int buttonsPerRow = 3;
+        readonly Vector2 buttonSize = new Vector2(40, 8);
+        AnimationClip _normalClip;
+        AnimationClip _highlightedClip;
+        
+        TableCell _modListTableCellInstance;
+        public event Action<MenuButton> pinButtonPushed;
+
+        protected override void DidActivate(bool firstActivation, ActivationType type)
+        {
+            try
+            {
+                if (firstActivation)
+                {
+                    if (_modListTableCellInstance == null)
+                    {
+                        var modMenuListItem = new GameObject("ModMenuListTableCell");
+
+                        var listItemRectTransform = modMenuListItem.AddComponent<RectTransform>();
+                        listItemRectTransform.anchorMin = new Vector2(0f, 0f);
+                        listItemRectTransform.anchorMax = new Vector2(1f, 1f);
+
+                        var horiz = modMenuListItem.AddComponent<HorizontalLayoutGroup>();
+                        horiz.spacing = 2f;
+                        horiz.childControlHeight = false;
+                        horiz.childControlWidth = false;
+                        horiz.childForceExpandWidth = false;
+                        horiz.childAlignment = TextAnchor.MiddleCenter;
+                        (horiz.transform as RectTransform).anchoredPosition = new Vector2(0, 0);
+                        horiz.padding = new RectOffset(0, 0, 4, 4);
+
+                        _modListTableCellInstance = modMenuListItem.gameObject.AddComponent<TableCell>();
+
+                        BeatSaberUI.CreateBackButton(rectTransform, () => backButtonPressed?.Invoke());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEPTION IN DidActivate: " + e);
+            }
+            base.DidActivate(firstActivation, type);
+
+            RectTransform container = (transform.Find("CustomListContainer") as RectTransform);
+            container.sizeDelta = new Vector2(150, 0);
+            container.anchoredPosition = new Vector2(0, 0);
+        }
+
+        internal void SetData(List<MenuButton> buttonData)
+        {
+            buttons = buttonData;
+        }
+        
+
+        public override float RowHeight()
+        {
+            return 12f;
+        }
+
+        public override int NumberOfRows()
+        {
+            return (int)Math.Ceiling((float)buttons.Count / buttonsPerRow);
+        }
+
+        public override TableCell CellForRow(int row)
+        {
+            TableCell _tableCell = Instantiate(_modListTableCellInstance);
+
+            for (int i = 0; i < buttonsPerRow; i++)
+            {
+                int index = row * buttonsPerRow + i;
+                if (buttons.Count > index)
+                {
+                    var menuButton = buttons.ElementAt(index);
+
+                    RectTransform container = new GameObject("container", typeof(RectTransform)).GetComponent<RectTransform>();
+                    container.SetParent(_tableCell.transform);
+                    container.sizeDelta = buttonSize;
+
+                    Button newButton = BeatSaberUI.CreateUIButton(container, "QuitButton", new Vector2(0,0), buttonSize ,menuButton.onClick, menuButton.text, menuButton.icon);
+                    newButton.GetComponentInChildren<HorizontalLayoutGroup>().padding = new RectOffset(6, 8, 0, 0);
+
+                    
+                    //  sub button
+                    var pinButton = BeatSaberUI.CreateUIButton(container, "QuitButton", new Vector2(-6,0), new Vector2(8,8), null, "", null);
+
+                    if (_highlightedClip == null) _highlightedClip = pinButton.GetComponent<ButtonStaticAnimations>().GetPrivateField<AnimationClip>("_highlightedClip");
+                    if (_normalClip == null) _normalClip = pinButton.GetComponent<ButtonStaticAnimations>().GetPrivateField<AnimationClip>("_normalClip");
+                    if (_normalClip == null) Console.WriteLine("============NORMAL CLIP NULL===========");
+
+                    PinButtonPushEffect(pinButton, menuButton);
+                    (pinButton.transform as RectTransform).anchorMin = new Vector2(1, 0.5f);
+                    (pinButton.transform as RectTransform).anchorMax = new Vector2(1, 0.5f);
+                    (pinButton.transform as RectTransform).anchoredPosition = new Vector2(-4, 0);
+                    pinButton.GetComponentInChildren<HorizontalLayoutGroup>().padding = new RectOffset(0, 0, 0, 0);
+                    pinButton.onClick.AddListener( () => { pinButtonPushed?.Invoke(menuButton); PinButtonPushEffect(pinButton, menuButton); });
+                }
+            }
+
+            return _tableCell;
+        }
+
+        void PinButtonPushEffect(Button pinButton, MenuButton menuButton)
+        {   
+            if (menuButton.pinned)
+            {
+                pinButton.SetButtonText("+");
+                pinButton.SetButtonTextSize(8);
+                pinButton.transform.Find("Wrapper/Stroke").gameObject.SetActive(true);
+                (pinButton.transform as RectTransform).Rotate(0, 0, 45);
+                pinButton.GetComponent<ButtonStaticAnimations>()?.SetPrivateField("_normalClip", _highlightedClip);
+                pinButton.GetComponent<ButtonStaticAnimations>()?.SetPrivateField("_highlightedClip", _normalClip);
+            } else
+            {
+                pinButton.SetButtonText("+");
+                pinButton.SetButtonTextSize(6);
+                pinButton.transform.Find("Wrapper/Stroke").gameObject.SetActive(false);
+                (pinButton.transform as RectTransform).localRotation = Quaternion.identity;
+                pinButton.GetComponent<ButtonStaticAnimations>()?.SetPrivateField("_normalClip", _normalClip);
+                pinButton.GetComponent<ButtonStaticAnimations>()?.SetPrivateField("_highlightedClip", _highlightedClip);
+            }
+        }
+    }
+}
--- MenuButton/MenuButtonUI.cs
+++ MenuButton/MenuButtonUI.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using CustomUI.BeatSaber;
+using CustomUI.Utilities;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using IllusionPlugin;
+using System.Linq;
+using VRUI;
 
 namespace CustomUI.MenuButton
 {
@@ -13,13 +18,18 @@ namespace CustomUI.MenuButton
         private static readonly WaitUntil _bottomPanelExists = new WaitUntil(() => GameObject.Find("MainMenuViewController/BottomPanel"));
         const int ButtonsPerRow = 4;
         const float RowSeparator = 9f;
-        
+
         private RectTransform bottomPanel;
         private RectTransform menuButtonsOriginal;
-
-        private int rowCount = 0;
+        
         private RectTransform currentRow;
-        private int buttonsInCurrentRow;
+        private List<RectTransform> rows;
+        private int buttonsInCurrentRow = ButtonsPerRow;
+
+        private List<MenuButton> buttonData;
+        private List<String> pinnedButtons;
+        private MenuButtonListViewController _menuButtonListViewController;
+        private CustomMenu _menuButtonListMenu;
 
         private static MenuButtonUI _instance = null;
         public static MenuButtonUI Instance
@@ -65,47 +75,186 @@ namespace CustomUI.MenuButton
 
         private void Init()
         {
-            // Find Menu buttons
-            buttonsInCurrentRow = ButtonsPerRow;
+            buttonData = new List<MenuButton>();
+            rows = new List<RectTransform>();
+
+            pinnedButtons = ModPrefs.GetString("CustomUI", "PinnedMenuButtons", "", true).Split(',').ToList();
+            
+            StartCoroutine(AddMenuButtonListButton());
+            
+            AddButton($"Kyles fault", null);
+            AddButton($"delete CMB", null);
+            AddButton($"Bacon", null);
+            AddButton($"owo", null);
+            AddButton($"what's this?", null);
+            AddButton($"ban", null);
+            AddButton($"gif.gif", null);
+            AddButton($"how instal sword", null);
+            AddButton($"ShameSaber", null);
+            AddButton($"NUT", null);
+            AddButton($"n i c e b o i", null);
+            AddButton($"buttonText", null);
+            AddButton($"RainModWhen", null);
+            AddButton($"Gnome", null);
+            AddButton($"BlueScreenPls", null);
+            AddButton($"(0_0)", null);
+            AddButton($"!", null);
+            AddButton($"Yes", null);
+            AddButton($"No", null);
+            AddButton($"Maybe", null);
+            AddButton($"I don't know", null);
+            AddButton($"repeat question", null);
+            AddButton($"code faster", null);
+            AddButton($"Give ChromaToggle", null);
         }
         
-        private RectTransform AddRow()
-        {
-            RectTransform newRow = RectTransform.Instantiate(menuButtonsOriginal, bottomPanel);
-            newRow.name = "CustomMenuButtonsRow" + rowCount.ToString();
-
-            foreach (Transform child in newRow)
-            {
-                child.name = String.Empty;
-                GameObject.Destroy(child.gameObject);
-            }
-            rowCount++;
-            newRow.anchoredPosition += Vector2.up * RowSeparator * rowCount;
-            return newRow;
-        }
-
-        private static IEnumerator AddButtonDelayed(string text, UnityAction onClick, Sprite icon)
+        private static IEnumerator AddMenuButtonListButton()
         {
             yield return _bottomPanelExists;
 
             lock (Instance)
             {
-                Instance.bottomPanel = GameObject.Find("MainMenuViewController/BottomPanel").transform as RectTransform;
-                Instance.menuButtonsOriginal = Instance.bottomPanel.Find("Buttons") as RectTransform;
-
-                if (Instance.buttonsInCurrentRow >= ButtonsPerRow)
-                {
-                    Instance.currentRow = Instance.AddRow();
-                    Instance.buttonsInCurrentRow = 0;
-                }
-                Button newButton = BeatSaberUI.CreateUIButton(Instance.currentRow as RectTransform, "QuitButton", onClick, text, icon);
-                Instance.buttonsInCurrentRow++;
+                var bottomPanel = GameObject.Find("MainMenuViewController/BottomPanel").transform as RectTransform;
+                var buttonRow = bottomPanel.Find("Buttons") as RectTransform;
+                
+                Button newButton = BeatSaberUI.CreateUIButton(buttonRow as RectTransform, "QuitButton", () => Instance.PresentList(), "Mods", null);
+                buttonRow.Find("QuitButton").SetAsLastSibling();
             }
+        }
+
+        private void PresentList()
+        {
+            if (_menuButtonListViewController == null)
+            {
+                _menuButtonListViewController = BeatSaberUI.CreateViewController<MenuButtonListViewController>();
+                _menuButtonListViewController.pinButtonPushed += PinButtonWasPushed;
+                _menuButtonListViewController.backButtonPressed += ListBackPressed;
+            }
+            _menuButtonListViewController.SetData(buttonData);
+
+            FlowCoordinator flowCoordinator = Resources.FindObjectsOfTypeAll<MainFlowCoordinator>().First();
+            flowCoordinator.InvokePrivateMethod("PresentViewController", new object[] { _menuButtonListViewController, null, false });
+
+            /*
+            if(_menuButtonListMenu == null)
+            {
+                _menuButtonListMenu = BeatSaberUI.CreateCustomMenu<CustomMenu>("Mods");
+                _menuButtonListMenu.SetMainViewController(_menuButtonListViewController, true);
+            }
+            _menuButtonListMenu.Present();
+            */
+        }
+
+        private void ListBackPressed()
+        {
+            FlowCoordinator flowCoordinator = Resources.FindObjectsOfTypeAll<MainFlowCoordinator>().First();
+            flowCoordinator.InvokePrivateMethod("DismissViewController", new object[] { _menuButtonListViewController, null, false });
+        }
+
+        private static IEnumerator AddButtonToMainMenuCoroutine(MenuButton button)
+        {
+            yield return _bottomPanelExists;
+
+            if (Instance.bottomPanel == null) Instance.bottomPanel = GameObject.Find("MainMenuViewController/BottomPanel").transform as RectTransform;
+            if (Instance.menuButtonsOriginal == null) Instance.menuButtonsOriginal = Instance.bottomPanel.Find("Buttons") as RectTransform;
+
+            lock (Instance)
+            {
+                Instance.AddButtonToMainMenu(button);
+            }
+        }
+
+        private void AddButtonToMainMenu(MenuButton button)
+        {
+            if (buttonsInCurrentRow >= ButtonsPerRow)
+            {
+                AddRow();
+            }
+
+            Button newButton = BeatSaberUI.CreateUIButton(currentRow, "QuitButton", button.onClick, button.text, button.icon);
+            newButton.GetComponentInChildren<HorizontalLayoutGroup>().padding = new RectOffset(6, 6, 0, 0);
+            newButton.name = button.text;
+            buttonsInCurrentRow++;
         }
 
         public static void AddButton(string buttonText, UnityAction onClick, Sprite icon = null)
         {
-            Instance.StartCoroutine(AddButtonDelayed(buttonText, onClick, icon));
+            bool pin = Instance.pinnedButtons.Contains(buttonText);
+
+            MenuButton menuButton = new MenuButton(buttonText, onClick, icon, pin);
+            Instance.buttonData.Add(menuButton);
+            if (menuButton.pinned)
+            {
+                Instance.StartCoroutine(AddButtonToMainMenuCoroutine(menuButton));
+            }
+        }
+
+        void PinButton(MenuButton menuButton)
+        {
+            if (menuButton.pinned) return;
+            menuButton.pinned = true;
+            if (!pinnedButtons.Contains(menuButton.text)) pinnedButtons.Add(menuButton.text);
+            ModPrefs.SetString("CustomUI", "PinnedMenuButtons", string.Join(",", pinnedButtons));
+            AddButtonToMainMenu(menuButton);
+        }
+
+        void UnpinButton(MenuButton menuButton)
+        {
+            if (!menuButton.pinned) return;
+            menuButton.pinned = false;
+            if(pinnedButtons.Contains(menuButton.text)) pinnedButtons.Remove(menuButton.text);
+            ModPrefs.SetString("CustomUI", "PinnedMenuButtons", string.Join(",", pinnedButtons));
+            Rebuild();
+        }
+
+        void PinButtonWasPushed(MenuButton button)
+        {
+            if (button.pinned)
+            {
+                UnpinButton(button);
+            } else
+            {
+                // todo: max pins
+                PinButton(button);
+            }
+        }
+
+        private void AddRow()
+        {
+            RectTransform newRow = Instantiate(menuButtonsOriginal, bottomPanel);
+            rows.Add(newRow);
+            currentRow = newRow;
+            buttonsInCurrentRow = 0;
+            newRow.name = "CustomMenuButtonsRow" + rows.Count.ToString();
+
+            foreach (Transform child in newRow)
+            {
+                child.name = String.Empty;
+                Destroy(child.gameObject);
+            }
+            newRow.anchoredPosition += Vector2.up * RowSeparator * rows.Count;
+        }
+
+        private void Rebuild()
+        {
+            Console.WriteLine(rows.Count);
+            while(rows.Count > 0)
+            {
+                Destroy(rows.First().gameObject);
+                rows.Remove(rows.First());
+            }
+            Console.WriteLine(rows.Count);
+            currentRow = null;
+            buttonsInCurrentRow = ButtonsPerRow;
+            rows.Clear();
+
+            foreach (MenuButton button in buttonData)
+            {
+                if (button.pinned)
+                {
+                    AddButtonToMainMenu(button);
+                }
+            }
         }
     }
 }
